e3e43222460772bf663f149b34a3f82e94c2afd4
tarunmandhan <mandhantarun@gmail.com>
2022-12-18 01:20:44 +0530

    buttons & div

---
+++ src/TodoList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>0</h1>
+          <div className="btn_div">
+            <button>Increment</button>
+            <button>Decrement</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TodoList;
